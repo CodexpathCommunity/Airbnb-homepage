10b128be5e9f6103053b1c98ca25c8541e906629
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-03-15 19:35:31 +0100

    Working on the airbnb home page

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an airbnb website clone by clever programmer.
+one very helpful resource you can find React projects on youtube is clever programmer.
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDK9rU4dQhvxZckA5eaLHhWryyHn1uVfl8",
+  authDomain: "clone-10151.firebaseapp.com",
+  databaseURL: "https://clone-10151.firebaseio.com",
+  projectId: "clone-10151",
+  storageBucket: "clone-10151.appspot.com",
+  messagingSenderId: "1019162013134",
+  appId: "1:1019162013134:web:1502dffbd5c47e0e5f8f9f",
+  measurementId: "G-QZXC9MKXMQ",
+};
